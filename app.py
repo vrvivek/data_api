d62b9d729fe9f5a46b7082baaa1efb1e71b5d14c
Rushi Reshamwala <reshamwalarushi786@gmail.com>
2021-09-26 18:09:55 +0530

    .

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = connexion.App(__name__, specification_dir='./')
 
 # Read the swagger.yml file to configure the endpoints
 app.add_api('swagger.yml')
-
+print('name',__name__)
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
